926cfa5a3f50dc9bdf65525d5c92daf823ab5c38
Dylan James Wagner <hello@dylanjameswagner.com>
2015-04-25 18:54:50 -0400

    cached plural

--- sidebar.php
+++ sidebar.php
@@ -1,8 +1,8 @@
-<?php // if (is_active_sidebar(strtolower(get_post_type_object(get_post_type())->label))) : ?>
+<?php $plural = get_post_type_object(get_post_type())->label; ?>
+<?php // if (is_active_sidebar(strtolower($plural))) : ?>
 
 			<section id="secondary"><div class="inner">
-
-<?php if (!dynamic_sidebar(strtolower(get_post_type_object(get_post_type())->label))) : ?>
+<?php if (!dynamic_sidebar(strtolower($plural))) : ?>
 
 				<?php // default content ?>
 				<?php get_search_form(); ?>
